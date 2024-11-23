9510dfae27b0dfffb88dd7413e1ca6e89d0997c6
Lucas Vandenbergh <>
2024-11-23 11:31:36 -0700

    add startpage

---
+++ startpage/linkConfig.js
@@ -0,0 +1,49 @@
+const linkConfig = [
+  {
+    category: "social/",
+    links: [
+      {
+        showAs: "reddit",
+        link: "https://www.reddit.com",
+      },
+    ],
+  },
+  {
+    category: "media/",
+    links: [
+      {
+        showAs: "youtube",
+        link: "https://www.youtube.com",
+      },
+      {
+        showAs: "twitch",
+        link: "https://www.twitch.tv",
+      },
+      {
+        showAs: "spotify",
+        link: "https://open.spotify.com",
+      },
+      {
+        showAs: "soundcloud",
+        link: "https://www.soudcloud.com",
+      },
+    ],
+  },
+  {
+    category: "dnd/",
+    links: [
+      {
+        showAs: "dicecloud",
+        link: "https://dicecloud.com",
+      },
+      {
+        showAs: "DND beyond",
+        link: "https://dndbeyond.com",
+      },
+      {
+        showAs: "roll20",
+        link: "https://roll20.net",
+      },
+    ],
+  },
+];
