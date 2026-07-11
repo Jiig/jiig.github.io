9b9aaa3d2fb916591328ee6dd38005d3c8e46d09
Lucas Vandenbergh <1923289+Jiig@users.noreply.github.com>
2026-07-11 11:39:15 -0600

    Switch to astro content for config file, added basic search bar (#3)
    * Switch to astro content for config file, added basic search bar
    
    * PR fixes

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import {defineCollection} from 'astro:content';
+import { glob } from 'astro/loaders'
+
+import {LinksSchema} from './content/links/types'
+
+const links = defineCollection({
+  loader: glob({pattern: "**/*.{yaml,yml}", base:"./src/content/links"}),
+  schema: LinksSchema,
+});
+
+export const collections = {
+  links
+};
---
+++ src/content/links/types.ts
@@ -0,0 +1,20 @@
+import { z } from 'astro/zod'
+
+export const LinkSchema = z.object({
+  name: z.string(),
+  iconName: z.string(),
+  url: z.url()
+});
+
+export const SectionSchema = z.object({
+  name: z.string(),
+  links: z.array(LinkSchema),
+});
+
+export const LinksSchema = z.object({
+    sections: z.array(SectionSchema)
+});
+
+export type Section = z.infer<typeof SectionSchema>
+export type Links = z.infer<typeof LinksSchema>
+export type Link = z.infer<typeof LinkSchema>
